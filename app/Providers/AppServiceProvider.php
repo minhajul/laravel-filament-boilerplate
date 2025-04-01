7e5616f783416288cc88393ef7453f800f73db91
minhajul <kmsohelinfo@gmail.com>
2025-04-01 10:54:43 +0600

    Updated

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Client\RequestException;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +21,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        DB::prohibitDestructiveCommands(
+            $this->app->isProduction()
+        );
+
+        if (config('app.debug')) {
+            RequestException::dontTruncate();
+        }
     }
 }
