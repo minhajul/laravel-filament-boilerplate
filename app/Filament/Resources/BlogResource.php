f7622088e0a11f48be782954b5e0ff9f6aba9249
minhajul <kmsohelinfo@gmail.com>
2025-04-01 11:02:32 +0600

    Change the menu icon

--- app/Filament/Resources/BlogResource.php
+++ app/Filament/Resources/BlogResource.php
@@ -16,7 +16,7 @@ class BlogResource extends Resource
 {
     protected static ?string $model = Blog::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-clipboard-document-list';
 
     public static function form(Form $form): Form
     {
