8a2bb5e8262ce467a31b24a4112e10f17dba7661
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-01-03 22:37:36 +0600

    Updated

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -4,8 +4,6 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-uses(RefreshDatabase::class);
-
 it('returns a successful response', function () {
     $this->get('/')
         ->assertStatus(200);
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -2,15 +2,7 @@
 
 namespace Tests\Unit;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use PHPUnit\Framework\TestCase;
-
-class ExampleTest extends TestCase
-{
-    use RefreshDatabase;
-
-    public function test_that_true_is_true(): void
-    {
-        $this->assertTrue(true);
-    }
-}
+it('returns a successful response', function () {
+    $this->get('/')
+        ->assertStatus(200);
+});
